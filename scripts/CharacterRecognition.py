96c942a6c6ac84ab7c101954a2ae0033be342bcf
G00401808@atu.ie <G00401808@atu.ie>
2024-04-26 01:21:43 +0100

    populated the if statements so that more rooms found can return an associated link to mazeaps

--- scripts/CharacterRecognition.py
+++ scripts/CharacterRecognition.py
@@ -27,28 +27,28 @@ def ScanImageForText(img):
 
     #re.search() searches a variable with a regex expression
     if re.search("CR1|cr1", textFromImage):
-        detectedRoomLink = "CR1"
+        detectedRoomLink = "https://use.mazemap.com/#v=1&campusid=954&zlevel=2&center=-9.010505,53.278868&zoom=18.8&sharepoitype=poi&sharepoi=1001604875&showpoidetails=true"
 
     elif re.search("CR2|cr2", textFromImage):
-        detectedRoomLink = "CR2"
+        detectedRoomLink = "https://use.mazemap.com/#v=1&campusid=954&zlevel=2&center=-9.010505,53.278868&zoom=18.8&sharepoitype=poi&sharepoi=1001604863&showpoidetails=true"
 
     elif re.search("CR3|cr3", textFromImage):
-        detectedRoomLink = "CR3"
+        detectedRoomLink = "https://use.mazemap.com/#v=1&campusid=954&zlevel=2&center=-9.010505,53.278868&zoom=18.8&sharepoitype=poi&sharepoi=1001604796&showpoidetails=true"
 
     elif re.search("CR4|cr4", textFromImage):
-        detectedRoomLink = "CR4"
+        detectedRoomLink = "https://use.mazemap.com/#v=1&campusid=954&zlevel=2&center=-9.010505,53.278868&zoom=18.8&sharepoitype=poi&sharepoi=1001604795&showpoidetails=true"
 
     elif re.search("CR5|cr5|CRS", textFromImage):
         detectedRoomLink = "https://use.mazemap.com/#v=1&campusid=954&zlevel=2&center=-9.010442,53.278791&zoom=18.6&sharepoitype=poi&sharepoi=1001604868&showpoidetails=true"
 
     elif re.search("CR6|cr6", textFromImage):
-        detectedRoomLink = "CR6"
+        detectedRoomLink = "https://use.mazemap.com/#v=1&campusid=954&zlevel=1&center=-9.009998,53.278794&zoom=20.4&sharepoitype=poi&sharepoi=1001604718&showpoidetails=true"
 
     elif re.search("CR7|cr7", textFromImage):
-        detectedRoomLink = "CR7"
+        detectedRoomLink = "https://use.mazemap.com/#v=1&campusid=954&zlevel=2&center=-9.010505,53.278868&zoom=18.8&sharepoitype=poi&sharepoi=1001604806&showpoidetails=true"
 
     elif re.search("CR8|cr8", textFromImage):
-        detectedRoomLink = "CR8"
+        detectedRoomLink = "https://use.mazemap.com/#v=1&campusid=954&zlevel=2&center=-9.010505,53.278868&zoom=18.8&sharepoitype=poi&sharepoi=1001604822&showpoidetails=true"
 
     else:
         detectedRoomLink = "Room not found!"
